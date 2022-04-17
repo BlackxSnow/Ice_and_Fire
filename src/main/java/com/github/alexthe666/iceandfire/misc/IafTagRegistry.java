7cf25d33ba49b4ed95e866cc494dc8650c76b5b2
Black--Snow <calvinsoueid@gmail.com>
2022-04-17 19:53:47 +1000

    replace new TagKey with TagKey.create

--- src/main/java/com/github/alexthe666/iceandfire/misc/IafTagRegistry.java
+++ src/main/java/com/github/alexthe666/iceandfire/misc/IafTagRegistry.java
@@ -12,17 +12,17 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class IafTagRegistry {
-    public static final TagKey<Item> DRAGON_ARROWS = new TagKey<Item>(Registry.ITEM_REGISTRY, new ResourceLocation("iceandfire", "dragon_arrows"));
-    public static final TagKey<Block> MYRMEX_HARVESTABLES = new TagKey<>(Registry.BLOCK_REGISTRY, new ResourceLocation("iceandfire", "myrmex_harvestables"));
-    public static final TagKey<EntityType<?>> CHICKENS = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "chickens"));
-    public static final TagKey<EntityType<?>> FEAR_DRAGONS = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "chickens"));
-    public static final TagKey<EntityType<?>> SCARES_COCKATRICES = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "scares_cockatrices"));
-    public static final TagKey<EntityType<?>> SHEEP = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "sheep"));
-    public static final TagKey<EntityType<?>> VILLAGERS = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "villagers"));
-    public static final TagKey<EntityType<?>> ICE_DRAGON_TARGETS = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "ice_dragon_targets"));
-    public static final TagKey<EntityType<?>> FIRE_DRAGON_TARGETS = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "fire_dragon_targets"));
-    public static final TagKey<EntityType<?>> LIGHTNING_DRAGON_TARGETS = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "lightning_dragon_targets"));
-    public static final TagKey<EntityType<?>> CYCLOPS_UNLIFTABLES = new TagKey<>(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "cyclops_unliftables"));
-    public static final TagKey<MobEffect> BLINDED = new TagKey<>(Registry.MOB_EFFECT_REGISTRY, new ResourceLocation("iceandfire", "blinded"));
+    public static final TagKey<Item> DRAGON_ARROWS = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("iceandfire", "dragon_arrows"));
+    public static final TagKey<Block> MYRMEX_HARVESTABLES = TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation("iceandfire", "myrmex_harvestables"));
+    public static final TagKey<EntityType<?>> CHICKENS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "chickens"));
+    public static final TagKey<EntityType<?>> FEAR_DRAGONS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "chickens"));
+    public static final TagKey<EntityType<?>> SCARES_COCKATRICES = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "scares_cockatrices"));
+    public static final TagKey<EntityType<?>> SHEEP = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "sheep"));
+    public static final TagKey<EntityType<?>> VILLAGERS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "villagers"));
+    public static final TagKey<EntityType<?>> ICE_DRAGON_TARGETS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "ice_dragon_targets"));
+    public static final TagKey<EntityType<?>> FIRE_DRAGON_TARGETS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "fire_dragon_targets"));
+    public static final TagKey<EntityType<?>> LIGHTNING_DRAGON_TARGETS = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "lightning_dragon_targets"));
+    public static final TagKey<EntityType<?>> CYCLOPS_UNLIFTABLES = TagKey.create(Registry.ENTITY_TYPE_REGISTRY, new ResourceLocation("iceandfire", "cyclops_unliftables"));
+    public static final TagKey<MobEffect> BLINDED = TagKey.create(Registry.MOB_EFFECT_REGISTRY, new ResourceLocation("iceandfire", "blinded"));
 
 }
